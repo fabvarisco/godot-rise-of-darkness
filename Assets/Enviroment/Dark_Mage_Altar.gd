deb0166d104e51059f72af23ab3638591bd9979f
Fabricio Varisco <fabricio.varisco@outlook.com>
2025-08-10 17:58:51 -0300

    add dark mages altars

--- Assets/Enviroment/Dark_Mage_Altar.gd
+++ Assets/Enviroment/Dark_Mage_Altar.gd
@@ -2,16 +2,21 @@ extends StaticBody2D
 
 export(String, "Fire_Mage","Dark_Mage", "Necromancer", "Dark_Lord" ) var type
 
+onready var sprite = $Sprite
+
+
 func _ready():
+	sprite.frame = 0
 	match type:
 		"Fire_Mage":
-			pass
+			if Global.dark_mages.fire_mage:
+				sprite.frame = 6
 		"Dark_Mage":
-			pass
+			if Global.dark_mages.dark_mage:
+				sprite.frame = 1
 		"Necromancer":
-			pass
-		"Dark_Lord":
-			pass
+			if Global.dark_mages.necromancer:
+				sprite.frame = 8
 
 func _input(_event):
 	if _event.is_action_pressed("action_3"):
--- Assets/World/Dungeon/Scripts/Hide_Stair.gd
+++ Assets/World/Dungeon/Scripts/Hide_Stair.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var entities = get_parent().get_node("Entities").get_children()
 
 func _process(_delta):
-	if get_parent().get_node("Entities").get_children().size() >= 1:
+	if get_parent().get_node("Entities").get_children().size() == 1:
 		$Stair.visible = false
 		$Stair/CollisionShape2D.disabled = true
 	else:
